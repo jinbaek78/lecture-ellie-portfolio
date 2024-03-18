f29e1950a460fbdb0271a9a38762950e7f1939e5
jinbaek <jinmb0602@gmail.com>
2024-03-18 20:46:43 +0900

    feat: implement arrow icon toggle on scroll within the home section

--- src/main.js
+++ src/main.js
@@ -14,3 +14,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// arrow: make arrow button toggle
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
