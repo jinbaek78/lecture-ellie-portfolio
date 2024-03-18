b1533e0e5d230e3917c6b8e9a05c9f289585fe9f
jinbaek <jinmb0602@gmail.com>
2024-03-18 22:18:18 +0900

    feat: implement menu bar toggling in mobile envirionments

--- src/main.js
+++ src/main.js
@@ -24,3 +24,33 @@ document.addEventListener('scroll', () => {
     arrowUp.style.opacity = 0;
   }
 });
+
+/**
+ * ✅1. when the screen is less than 768px
+ *  header menu bar off
+ *  toggle bars icon on
+ *
+ * ✅2. when the toggle bars is clicked
+ *  show menu bar
+ *
+ * ✅3. when the toggle bars is clicked again
+ *  disappear menu bar
+ *
+ * ☑️4. when the click a menu on the menu bar
+ *  move to the section clicked and
+ *  disappear menu bar
+ */
+
+const toggleBar = document.querySelector('.header__bars');
+const menuBar = document.querySelector('.header__menu');
+toggleBar.addEventListener('click', () => {
+  console.log('clicked');
+  console.log(menuBar);
+  menuBar.classList.toggle('header__menu--show');
+});
+
+menuBar.addEventListener('click', () => {
+  console.log('clicked');
+  console.log(menuBar);
+  menuBar.classList.toggle('header__menu--show');
+});
