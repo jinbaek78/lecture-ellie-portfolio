a2afbb022cda3a1f400c2419bc8938ee84a2aff5
jinbaek <jinmb0602@gmail.com>
2024-03-18 14:12:20 +0900

    feat: implement scrolling-based opacity adjustment for home

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight + 10) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+const home = document.querySelector('#home');
+const homeContainer = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+const criterionValue = Math.ceil(homeHeight * 0.1);
+document.addEventListener('scroll', () => {
+  const CORRECTED_VALUE = 0.2;
+  const scrollY = Math.ceil(window.scrollY / criterionValue);
+  const opacity = Number((1 - scrollY * 0.1 - CORRECTED_VALUE).toFixed(1));
+  homeContainer.style.opacity = `${opacity}`;
 });
