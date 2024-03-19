57318d2c6fb8b1e95896190e831c9f69d8651312
jinbaek <jinmb0602@gmail.com>
2024-03-19 23:37:04 +0900

    feat: implement projects filtering when clicked a category

--- src/project.js
+++ src/project.js
@@ -1,1 +1,45 @@
 'use strict';
+
+/**
+ * filtering projects
+ * when I click a category,
+ *  1. emphasize category color selected
+ *  2. show only projects related that category
+ *    (3.transition: appear from bottom)
+ *
+ *
+ */
+
+// const CATEGORY_LIST = ['all', 'frontend', 'backend', 'mobile'];
+const CLASS_CATEGORY_SELECTED = 'category--selected';
+const CLASS_PROJECT_OFF = 'project--off';
+const categories = document.querySelector('.categories');
+const projects = document.querySelector('.projects');
+let previousSelected = document.querySelector('[data-category="all"]');
+
+categories.addEventListener('click', (e) => {
+  const selectedCategory = e.target?.dataset?.category ?? 'all';
+  if (selectedCategory) {
+    const currentSelected = document.querySelector(
+      `[data-category="${selectedCategory}"]`
+    );
+    previousSelected?.classList.remove(CLASS_CATEGORY_SELECTED);
+    currentSelected?.classList.add(CLASS_CATEGORY_SELECTED);
+    previousSelected = currentSelected;
+  }
+
+  for (let i = 0; i < projects.children.length; i++) {
+    projects.children[i].classList.remove(CLASS_PROJECT_OFF);
+  }
+
+  if (selectedCategory === 'all') {
+    return;
+  }
+
+  for (let i = 0; i < projects.children.length; i++) {
+    const childrenCategory = projects.children[i]?.dataset?.category;
+    if (childrenCategory !== selectedCategory) {
+      projects.children[i].classList.add(CLASS_PROJECT_OFF);
+    }
+  }
+});
