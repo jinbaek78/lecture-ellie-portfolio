9d9ac07c8c56ec8d310a4d0d5f17c75b6255c6b9
jinbaek <jinmb0602@gmail.com>
2024-03-20 10:47:25 +0900

    feat: implement filtering of projects when a category is selected

--- src/project.js
+++ src/project.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// handling logic related to this project filtering
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+
+  // re-set active menu
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  event.target.classList.add('category--selected');
+
+  // project filtering
+  projects.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+  projectsContainer.classList.add('anim-out');
+});
